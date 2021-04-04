f803721ea024b7aeab39f104910c0b6e25b0454a
maximilian-steiner <maximilian.steiner99@gmail.com>
2021-04-04 23:24:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/learningdiary3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.learningdiary3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+}
---
+++ app/src/main/java/com/example/learningdiary3/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.learningdiary3
+
+data class Movie(var title: String = "", var description: String = "", var rating: Float=0.0f, var genres: String= "", var creators: String = "", var actors: String = "")
